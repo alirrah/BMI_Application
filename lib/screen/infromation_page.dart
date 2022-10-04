f92f0a4b6213db1d86cea1e027f7a2d6b538a141
alirrah81 <alirrah81@gmail.com>
2022-10-04 12:56:09 +0330

    clear the defualt project

---
+++ lib/screen/infromation_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class informationPage extends StatefulWidget {
+  const informationPage({Key? key}) : super(key: key);
+
+  @override
+  State<informationPage> createState() => _informationPageState();
+}
+
+class _informationPageState extends State<informationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
